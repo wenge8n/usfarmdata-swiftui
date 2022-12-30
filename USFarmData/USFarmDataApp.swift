6bb9178d30b9265dc3a766fbfbe44c3bd51967ed
wenge8n <wenge8n@outlook.com>
2022-12-30 23:05:02 +0800

    Initial Commit

---
+++ USFarmData/USFarmDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  USFarmDataApp.swift
+//  USFarmData
+//
+//  Created by Admin on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct USFarmDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
