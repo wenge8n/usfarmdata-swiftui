0a5349482815bf036c572b1a4d9729236bace142
wenge8n <wenge8n@outlook.com>
2023-01-04 18:55:55 +0800

    add screen views

---
+++ USFarmData/Views/Auth/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  USFarmData
+//
+//  Created by Admin on 1/4/23.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+    }
+}
---
+++ USFarmData/Views/Home/HomeScreen.swift
@@ -0,0 +1,20 @@
+//
+//  HomeScreen.swift
+//  USFarmData
+//
+//  Created by Admin on 1/4/23.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreen()
+    }
+}
