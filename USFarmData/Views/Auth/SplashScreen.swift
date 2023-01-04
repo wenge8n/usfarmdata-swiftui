ac5eb270ef1d09b74677553e6586d45a38860319
wenge8n <wenge8n@outlook.com>
2023-01-04 22:47:53 +0800

    show splash

--- USFarmData/Views/Auth/SplashScreen.swift
+++ USFarmData/Views/Auth/SplashScreen.swift
@@ -8,11 +8,26 @@
 import SwiftUI
 
 struct SplashScreen: View {
+    @State private var showSplash = true
+    
     var body: some View {
-        Image("LogoWithTitle")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 240, height: 128)
+        VStack {
+            if self.showSplash {
+                Image("LogoWithTitle")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 240, height: 128)
+            } else {
+                LoginScreen()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                withAnimation {
+                    self.showSplash = false
+                }
+            }
+        }
     }
 }
 
