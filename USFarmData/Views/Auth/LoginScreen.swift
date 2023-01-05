ee6187ffaf693e13bd46b8bf9110d26ae55045a7
wenge8n <wenge8n@outlook.com>
2023-01-05 18:44:41 +0800

    update

--- USFarmData/Views/Auth/LoginScreen.swift
+++ USFarmData/Views/Auth/LoginScreen.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct LoginScreen: View {
+    @EnvironmentObject var userData: UserData
+    @State private var email: String = ""
+    @State private var password: String = ""
+    
     var body: some View {
-        Text("Login")
+        VStack(spacing: 0) {
+            Image("Logo")
+                .resizable()
+                .aspectRatio(10 / 9, contentMode: .fit)
+                .frame(height: 90)
+            Text("Sign in to your account")
+                .font(.system(size: 22))
+            
+            TextField("Email", text: $email)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .textFieldStyle(.roundedBorder)
+                .font(.system(size: 17))
+                .padding(.top, 25)
+            SecureField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+                .font(.system(size: 17))
+                .padding(.top, 15)
+        }
+        .padding(16)
     }
 }
 
