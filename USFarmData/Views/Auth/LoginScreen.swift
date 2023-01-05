2e92f079d2da888006b265d3a8f9d8958e2dc564
wenge8n <wenge8n@outlook.com>
2023-01-06 01:56:14 +0800

    login

--- USFarmData/Views/Auth/LoginScreen.swift
+++ USFarmData/Views/Auth/LoginScreen.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Alamofire
+import SVProgressHUD
 
 struct LoginScreen: View {
     @EnvironmentObject var userData: UserData
@@ -87,6 +89,64 @@ struct LoginScreen: View {
     }
     
     func loginTapped() {
+        let ipaddr = getIPAddress()
+        SVProgressHUD.show()
+        let parameters: [String: Any] = [
+            "UserName": email,
+            "Password": password,
+            "Userip": ipaddr
+        ]
+        AF.request(Constants.API_URL + "/mobapp/api/gettoken", method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .responseDecodable(of: [LoginResponse].self) { response in
+                switch response.result {
+                case .success(let response):
+                    print(response)
+                    let token = response[0].token
+                    if !token.isEmpty {
+                        print(token)
+                        AppUtils.saveAccessToken(token)
+                        userData.token = token
+                    } else {
+                        let message = response[0].errstr
+                        errorData.message = message
+                        errorData.showAlert = true
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    errorData.message = error.localizedDescription
+                    errorData.showAlert = true
+                }
+                SVProgressHUD.dismiss()
+        }
+    }
+    
+    func getIPAddress() -> String {
+        var address: String?
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            while ptr != nil {
+                defer { ptr = ptr?.pointee.ifa_next }
+
+                guard let interface = ptr?.pointee else { return "" }
+                let addrFamily = interface.ifa_addr.pointee.sa_family
+                if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+
+                    // wifi = ["en0"]
+                    // wired = ["en2", "en3", "en4"]
+                    // cellular = ["pdp_ip0","pdp_ip1","pdp_ip2","pdp_ip3"]
+
+                    let name: String = String(cString: (interface.ifa_name))
+                    if  name == "en0" || name == "en2" || name == "en3" || name == "en4" || name == "pdp_ip0" || name == "pdp_ip1" || name == "pdp_ip2" || name == "pdp_ip3" {
+                        var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                        getnameinfo(interface.ifa_addr, socklen_t((interface.ifa_addr.pointee.sa_len)), &hostname, socklen_t(hostname.count), nil, socklen_t(0), NI_NUMERICHOST)
+                        address = String(cString: hostname)
+                    }
+                }
+            }
+            freeifaddrs(ifaddr)
+        }
+        return address ?? ""
     }
 }
 
